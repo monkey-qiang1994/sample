c28621f0148558556a5275a6c1cdb2f41614ab20
monkey-qiang1994 <452056667@qq.com>
2018-12-30 22:51:10 +0800

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,14 @@ use Auth;
 
 class SessionsController extends Controller
 {
+
+	public function __construct()
+	{
+		$this->middleware('guest',[
+			'only' => ['create']
+		]);
+	}
+
     public function create()
     {
     	return view('sessions.create');
@@ -23,7 +31,7 @@ class SessionsController extends Controller
     	if(Auth::attempt($credentials,$request->has('remember'))){
     		//登陆成功后的相关操作
     		session()->flash('success','欢迎回来!');
-    		return redirect()->route('users.show',[Auth::user()]);
+    		return redirect()->intended(route('users.show',[Auth::user()]));
     		
     	}else{
     		//登陆失败后的相关操作
